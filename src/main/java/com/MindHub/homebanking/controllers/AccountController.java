4da7cafed1fa3e04846630cdaee7efd023d5399b
MRMartinez7 <ricardo90.08@gmail.com>
2023-08-24 10:33:02 -0300

    Task6 Complete(la anterior tenia archivos del front mal)

--- src/main/java/com/MindHub/homebanking/controllers/AccountController.java
+++ src/main/java/com/MindHub/homebanking/controllers/AccountController.java
@@ -1,18 +1,17 @@
 package com.MindHub.homebanking.controllers;
 
 import com.MindHub.homebanking.dto.AccountDTO;
+import com.MindHub.homebanking.dto.ClientDTO;
 import com.MindHub.homebanking.models.Account;
 import com.MindHub.homebanking.models.Client;
+import com.MindHub.homebanking.models.RolType;
 import com.MindHub.homebanking.repositories.AccountRepository;
 import com.MindHub.homebanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -42,5 +41,29 @@ public class AccountController {
         }else {
             return new ResponseEntity<>("Esta Account no te pertenece",HttpStatus.NOT_FOUND);
         }
+    }
+
+    @RequestMapping("/clients/current/accounts")
+    public ClientDTO getCurrentClient(Authentication authentication){
+        return new ClientDTO(clientRepository.findByEmail(authentication.getName()));
+    }
+
+    @PostMapping(path = "/accounts")
+    public ResponseEntity<Object> register(
+            @RequestParam String number, @RequestParam String lastName,
+            @RequestParam String email, @RequestParam String password) {
+
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty()) {
+
+            return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
+        }
+
+        if (clientRepository.findByEmail(email) !=  null) {
+
+            return new ResponseEntity<>("Name already in use", HttpStatus.FORBIDDEN);
+        }
+        clientRepository.save(new Client(firstName, lastName, email, passwordEncoder.encode(password), RolType.CLIENT));
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
     }
 }
